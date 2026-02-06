97588425857e12562dd7407b2068cf0db7edfeae
Luke Roberts <rober2lk@mail.uc.edu>
2026-02-06 10:38:00 -0500

    final

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     {
-        
+        double purchasePrice = 100.00; //simulating input
+        final double TAX_RATE = .05;
+        double salesTax = 0;
+        double totalPrice=0;
+        salesTax = purchasePrice*TAX_RATE;
+        totalPrice = purchasePrice + salesTax;
+        System.out.println("The sales tax is " + salesTax + " dollars and the total price is " + totalPrice + " dollars.");
     }
 }
